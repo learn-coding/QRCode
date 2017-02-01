c2bde71309d65fcd1a54bc77fb735b1e1905cfd2
niladric <c19niladri@gmail.com>
2017-02-01 17:56:37 +0530

    Updated qr code file to accomodate changes

--- qr_code.py
+++ qr_code.py
@@ -7,11 +7,13 @@ from flask_restful import reqparse, Resource
 #import werkzeug
 from werkzeug.utils import secure_filename
 
+from core import Core
 
 postargs = reqparse.RequestParser()
 
-postargs.add_argument('id', type=int, default=1)
+postargs.add_argument('sex', type=str, required=True)
 postargs.add_argument('name', type=str, required=True)
+postargs.add_argument('age', type=int, required=True)
 
 class QRCode(Resource):
 
@@ -19,7 +21,7 @@ class QRCode(Resource):
     pass
 	
   def get(self):
-    #curl cmd : curl -i -X GET -H "Content-Type: multipart/form-data" -F "file=@F:/QR_code/filename.png" http://127.0.0.1:8888/qrcode/upload
+    #curl cmd : curl -i -X GET -H "Content-Type: multipart/form-data" -F "file=@F:/QRCode/filename.png" http://127.0.0.1:8888/qrcode
     if 'file' not in request.files:
         flash('No file part')
         return "file not found"
@@ -37,14 +39,18 @@ class QRCode(Resource):
         #return "FIle successfully uploaded"
         qr = qrtools.QR()
         qr.decode(filepath)
-        return qr.data
+        id_complete_data = Core.get_instance().read_data_from_db(qr.data)
+        return id_complete_data
 
 
   def post(self):
-    #curl cmd : curl --data "id=98742&name='niladri'" http://127.0.0.1:8888/qrcode
+    #curl cmd : curl -i -H "Content-Type: application/json" -X POST -d "{\"name\" : \"Niladri\", \"sex\" : \"M\", \"age\" : 31}" http://127.0.0.1:8888/qrcode
     qr_info = postargs.parse_args()
-    input = 'name: ' + 	qr_info['name'] + '\n ID: ' + str(qr_info['id'])
-    img = qrcode.make(input)
+    qr_info['user_id'] = Core.get_instance().create_random()
+    #input = 'name: ' + 	qr_info['name'] + '\n ID: ' + str(qr_info['id'])
+    #print qr_info['user_id']
+    img = qrcode.make(qr_info['user_id'])
+    Core.get_instance().write_data_to_db(qr_info)
     img.save("filename.png")
     msg = 'QR code created successfully' #% (poolName)
     return {'status': "true", 'message': msg}, 201
